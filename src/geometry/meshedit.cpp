0401a977733eec6e055ea655599e6de6704001d3
XLXXLXXLX <1294694867@qq.com>
2023-10-10 23:19:35 +0800

    2023-10-10

--- src/geometry/meshedit.cpp
+++ src/geometry/meshedit.cpp
@@ -35,20 +35,170 @@ bool operator<(const HalfedgeMesh::EdgeRecord& a, const HalfedgeMesh::EdgeRecord
 
 optional<Edge*> HalfedgeMesh::flip_edge(Edge* e)
 {
-    (void)e;
-    return std::nullopt;
+    // 要用到的半边
+    Halfedge* h12 = e->halfedge;
+    Halfedge* h21 = h12->inv;
+    Halfedge* h23 = h12->next;
+    Halfedge* h31 = h23->next;
+    Halfedge* h14 = h21->next;
+    Halfedge* h42 = h14->next;
+    auto h43      = h12;
+    auto h34      = h21;
+    // 要用到的顶点
+    // v1 and v2 are vertices along the edge
+    Vertex* v1 = h12->from;
+    Vertex* v2 = h21->from;
+    // v3 and v4 are vertices opposite the edge
+    Vertex* v3 = h31->from;
+    Vertex* v4 = h42->from;
+    // 要用到的面片
+    Face* f1 = h12->face;
+    Face* f2 = h21->face;
+
+    f1 = new_face(f1->is_boundary || f2->is_boundary);
+    f2 = new_face(f1->is_boundary || f2->is_boundary);
+
+    f1->halfedge = h12;
+    f2->halfedge = h21;
+    e->halfedge  = h12;
+    // 重新连接各基本元素
+    v1->halfedge = h14;
+    v2->halfedge = h23;
+    h43->set_neighbors(h31, h14, h21, v4, e, f1);
+    h34->set_neighbors(h42, h23, h12, v3, e, f2);
+    h31->set_neighbors(h14, h43, h31->inv, v3, h31->edge, f1);
+    h14->set_neighbors(h43, h31, h14->inv, v1, h14->edge, f1);
+    h42->set_neighbors(h23, h34, h42->inv, v4, h42->edge, f2);
+    h23->set_neighbors(h34, h42, h23->inv, v2, h23->edge, f2);
+
+    return e;
+    // }
 }
 
 optional<Vertex*> HalfedgeMesh::split_edge(Edge* e)
 {
-    (void)e;
-    return std::nullopt;
+    // 要用到的半边
+    auto h12 = e->halfedge;
+    auto h21 = h12->inv;
+    auto h23 = h12->next;
+    auto h31 = h23->next;
+    auto h14 = h21->next;
+    auto h42 = h14->next;
+    auto v1  = h12->from;
+    auto v2  = h21->from;
+    auto v3  = h31->from;
+    auto v4  = h42->from;
+    auto f1  = h12->face;
+    auto f2  = h21->face;
+
+    auto vn = new_vertex();
+    vn->pos = e->center();
+
+    auto h1n  = h12;
+    auto h2n  = h21;
+    auto h3n  = new_halfedge();
+    auto h4n  = new_halfedge();
+    auto hn1  = new_halfedge();
+    auto hn2  = new_halfedge();
+    auto hn3  = new_halfedge();
+    auto hn4  = new_halfedge();
+    auto e1n  = e;
+    auto e2n  = new_edge();
+    auto e3n  = new_edge();
+    auto e4n  = new_edge();
+    auto fn31 = new_face(f1->is_boundary);
+    auto fn23 = new_face(f1->is_boundary);
+    auto fn14 = new_face(f2->is_boundary);
+    auto fn42 = new_face(f2->is_boundary);
+    // 初始化edge
+    e1n->halfedge = hn1;
+    e2n->halfedge = hn2;
+    e3n->halfedge = hn3;
+    e4n->halfedge = hn4;
+    // 初始化face
+    fn31->halfedge = h31;
+    fn14->halfedge = h14;
+    fn23->halfedge = h23;
+    fn42->halfedge = h42;
+    // 连接halfedge
+    // h->set_netghbors(next, prev, inv, vertex, edge, face);
+    hn1->set_neighbors(h14, h4n, h1n, vn, e1n, fn14);
+    hn2->set_neighbors(h23, h3n, h2n, vn, e2n, fn23);
+    hn3->set_neighbors(h31, h1n, h3n, vn, e3n, fn31);
+    hn4->set_neighbors(h42, h2n, h4n, vn, e4n, fn42);
+    h1n->set_neighbors(hn3, h31, hn1, v1, e1n, fn31);
+    h2n->set_neighbors(hn4, h42, hn2, v2, e2n, fn42);
+    h3n->set_neighbors(hn2, h23, hn3, v3, e3n, fn23);
+    h4n->set_neighbors(hn1, h14, hn4, v4, e4n, fn14);
+    h14->set_neighbors(h4n, hn1, h14->inv, v1, h14->edge, fn14);
+    h23->set_neighbors(h3n, hn2, h23->inv, v2, h23->edge, fn23);
+    h31->set_neighbors(h1n, hn3, h31->inv, v3, h31->edge, fn31);
+    h42->set_neighbors(h2n, hn4, h42->inv, v4, h42->edge, fn42);
+
+    vn->halfedge = hn1;
+
+    return vn;
 }
 
 optional<Vertex*> HalfedgeMesh::collapse_edge(Edge* e)
 {
-    (void)e;
-    return std::nullopt;
+    // 要用到的半边
+    auto h12 = e->halfedge;
+    auto h21 = h12->inv;
+    auto h23 = h12->next;
+    auto h32 = h23->inv;
+    auto h31 = h12->prev;
+    auto h13 = h31->inv;
+    auto h14 = h21->next;
+    auto h41 = h14->inv;
+    auto h42 = h21->prev;
+    auto h24 = h42->inv;
+
+    auto h1     = h13;
+    auto h2     = h24;
+    auto hinv1  = h32;
+    auto hinv2  = h41;
+    auto hfrom2 = hinv1->next;
+    auto hfrom1 = hinv2->next;
+    // 要用到的边
+    auto e1n = h1->edge;
+    auto e2n = h2->edge;
+
+    e1n->halfedge = h1;
+    e2n->halfedge = h2;
+    // 要用到的顶点
+    auto v1 = h13->from;
+    auto v2 = h24->from;
+    auto v3 = h32->from;
+    auto v4 = h41->from;
+    erase(v1);
+    erase(v2);
+    // v1->halfedge = nullptr;
+    // v2->halfedge = nullptr;
+    v3->halfedge = hinv1;
+    v4->halfedge = hinv2;
+
+    auto vn      = new_vertex();
+    vn->pos      = (v3->pos + v4->pos) / 2;
+    vn->halfedge = h1;
+    h1->set_neighbors(h1->next, h1->prev, hinv1, vn, e1n, h1->face);
+    h2->set_neighbors(h2->next, h2->prev, hinv2, vn, e2n, h2->face);
+    hinv1->set_neighbors(hinv1->next, hinv1->prev, h1, v3, e1n, hinv1->face);
+    hinv2->set_neighbors(hinv2->next, hinv2->prev, h2, v4, e2n, hinv2->face);
+    erase(h23);
+    erase(h31);
+    erase(h14);
+    erase(h42);
+    while (hfrom2 != h2) {
+        hfrom2->from = vn;
+        hfrom2       = hfrom2->inv->next;
+    }
+    while (hfrom1 != h1) {
+        hfrom1->from = vn;
+        hfrom1       = hfrom1->inv->next;
+    }
+    erase(e);
+    return vn;
 }
 
 void HalfedgeMesh::loop_subdivide()

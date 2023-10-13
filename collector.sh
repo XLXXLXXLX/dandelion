75e6dae00a3f21e2d1a37fe0aada8da5578732f6
XLXXLXXLX <1294694867@qq.com>
2023-10-13 11:27:59 +0800

    2023-10-13 isotropic_remesh(unfinished)

---
+++ collector.sh
@@ -0,0 +1,7 @@
+# !/bin/zsh
+
+mkdir -p done
+zip -r src.zip src
+mv src.zip done
+mv screenshots.zip done
+cp modifications.cpp done
--- src/geometry/meshedit.cpp
+++ src/geometry/meshedit.cpp
@@ -1,8 +1,10 @@
 #include "halfedge.h"
 
 #include <cstddef>
+#include <iterator>
 #include <set>
 #include <map>
+#include <sys/types.h>
 #include <vector>
 #include <string>
 
@@ -36,6 +38,8 @@ bool operator<(const HalfedgeMesh::EdgeRecord& a, const HalfedgeMesh::EdgeRecord
 
 optional<Edge*> HalfedgeMesh::flip_edge(Edge* e)
 {
+    if (e->on_boundary())
+        return optional<Edge*>{};
     // 要用到的半边
     Halfedge* h12 = e->halfedge;
     Halfedge* h21 = h12->inv;
@@ -58,16 +62,15 @@ optional<Edge*> HalfedgeMesh::flip_edge(Edge* e)
 
     auto fn1 = new_face(f1->is_boundary || f2->is_boundary);
     auto fn2 = new_face(f1->is_boundary || f2->is_boundary);
-    erase(f1);
-    erase(f2);
+    erase(f1), erase(f2);
     fn1->halfedge = h12;
     fn2->halfedge = h21;
     e->halfedge   = h12;
     // 重新连接各基本元素
     v1->halfedge = h14;
     v2->halfedge = h23;
-    h43->set_neighbors(h31, h14, h21, v4, e, fn1);
-    h34->set_neighbors(h42, h23, h12, v3, e, fn2);
+    h43->set_neighbors(h31, h14, h34, v4, e, fn1);
+    h34->set_neighbors(h42, h23, h43, v3, e, fn2);
     h31->set_neighbors(h14, h43, h31->inv, v3, h31->edge, fn1);
     h14->set_neighbors(h43, h31, h14->inv, v1, h14->edge, fn1);
     h42->set_neighbors(h23, h34, h42->inv, v4, h42->edge, fn2);
@@ -79,125 +82,176 @@ optional<Edge*> HalfedgeMesh::flip_edge(Edge* e)
 
 optional<Vertex*> HalfedgeMesh::split_edge(Edge* e)
 {
-    // 要用到的半边
-    auto h12 = e->halfedge;
-    auto h21 = h12->inv;
-    auto h23 = h12->next;
-    auto h31 = h23->next;
-    auto h14 = h21->next;
-    auto h42 = h14->next;
-    auto v1  = h12->from;
-    auto v2  = h21->from;
-    auto v3  = h31->from;
-    auto v4  = h42->from;
-    auto f1  = h12->face;
-    auto f2  = h21->face;
-
-    auto vn    = new_vertex();
-    vn->pos    = e->center();
-    vn->is_new = true;
-
-    auto h1n  = h12;
-    auto h2n  = h21;
-    auto h3n  = new_halfedge();
-    auto h4n  = new_halfedge();
-    auto hn1  = new_halfedge();
-    auto hn2  = new_halfedge();
-    auto hn3  = new_halfedge();
-    auto hn4  = new_halfedge();
-    auto e1n  = e;
-    auto e2n  = new_edge();
-    auto e3n  = new_edge();
-    auto e4n  = new_edge();
-    auto fn31 = new_face(f1->is_boundary);
-    auto fn23 = new_face(f1->is_boundary);
-    auto fn14 = new_face(f2->is_boundary);
-    auto fn42 = new_face(f2->is_boundary);
-    // 初始化edge
+    auto h12      = e->halfedge;
+    auto h21      = h12->inv;
+    auto v1       = h12->from;
+    auto v2       = h21->from;
+    auto vn       = new_vertex();
+    vn->pos       = e->center();
+    vn->is_new    = true;
+    auto h1n      = h12;
+    auto h2n      = h21;
+    auto hn1      = new_halfedge();
+    auto hn2      = new_halfedge();
+    auto e1n      = e;
+    auto e2n      = new_edge();
     e1n->is_new   = false;
     e2n->is_new   = false;
-    e3n->is_new   = true;
-    e4n->is_new   = true;
     e1n->halfedge = hn1;
     e2n->halfedge = hn2;
-    e3n->halfedge = hn3;
-    e4n->halfedge = hn4;
-    // 初始化face
-    erase(f1);
-    erase(f2);
-    fn31->halfedge = h31;
-    fn14->halfedge = h14;
-    fn23->halfedge = h23;
-    fn42->halfedge = h42;
-    // 连接halfedge
-    // h->set_netghbors(next, prev, inv, vertex, edge, face);
-    hn1->set_neighbors(h14, h4n, h1n, vn, e1n, fn14);
-    hn2->set_neighbors(h23, h3n, h2n, vn, e2n, fn23);
-    hn3->set_neighbors(h31, h1n, h3n, vn, e3n, fn31);
-    hn4->set_neighbors(h42, h2n, h4n, vn, e4n, fn42);
-    h1n->set_neighbors(hn3, h31, hn1, v1, e1n, fn31);
-    h2n->set_neighbors(hn4, h42, hn2, v2, e2n, fn42);
-    h3n->set_neighbors(hn2, h23, hn3, v3, e3n, fn23);
-    h4n->set_neighbors(hn1, h14, hn4, v4, e4n, fn14);
-    h14->set_neighbors(h4n, hn1, h14->inv, v1, h14->edge, fn14);
-    h23->set_neighbors(h3n, hn2, h23->inv, v2, h23->edge, fn23);
-    h31->set_neighbors(h1n, hn3, h31->inv, v3, h31->edge, fn31);
-    h42->set_neighbors(h2n, hn4, h42->inv, v4, h42->edge, fn42);
+    if (h21->is_boundary()) {
+        logger->info("boundary......");
+        // 如果h21在边上，那么h21->face即为虚假面
+        hn1->set_neighbors(h21->next, h2n, h1n, vn, e1n, h21->face);
+        h2n->set_neighbors(hn1, h21->prev, hn2, v2, e2n, h21->face);
+        h21->face->halfedge = hn1;
+    } else {
+        // 如果h21不在边上，则正常处理
+        auto h14      = h21->next;
+        auto h42      = h14->next;
+        auto v4       = h42->from;
+        auto f2       = h21->face;
+        auto h4n      = new_halfedge();
+        auto hn4      = new_halfedge();
+        auto fn14     = new_face(f2->is_boundary);
+        auto fn42     = new_face(f2->is_boundary);
+        auto e4n      = new_edge();
+        e4n->is_new   = true;
+        e4n->halfedge = hn4;
+        erase(f2);
+        fn14->halfedge = h14;
+        fn42->halfedge = h42;
+        hn1->set_neighbors(h14, h4n, h1n, vn, e1n, fn14);
+        hn4->set_neighbors(h42, h2n, h4n, vn, e4n, fn42);
+        h2n->set_neighbors(hn4, h42, hn2, v2, e2n, fn42);
+        h4n->set_neighbors(hn1, h14, hn4, v4, e4n, fn14);
+        h14->set_neighbors(h4n, hn1, h14->inv, v1, h14->edge, fn14);
+        h42->set_neighbors(h2n, hn4, h42->inv, v4, h42->edge, fn42);
+    }
+    if (h12->is_boundary()) {
+        logger->info("boundary......");
+        h1n->set_neighbors(hn2, h12->prev, hn1, v1, e1n, h12->face);
+        hn2->set_neighbors(h12->next, h1n, h2n, vn, e2n, h12->face);
+        h12->face->halfedge = h1n;
+    } else {
+        auto h23      = h12->next;
+        auto h31      = h23->next;
+        auto v3       = h31->from;
+        auto f1       = h12->face;
+        auto h3n      = new_halfedge();
+        auto hn3      = new_halfedge();
+        auto fn31     = new_face(f1->is_boundary);
+        auto fn23     = new_face(f1->is_boundary);
+        auto e3n      = new_edge();
+        e3n->is_new   = true;
+        e3n->halfedge = hn3;
+        erase(f1);
+        fn31->halfedge = h31;
+        fn23->halfedge = h23;
+        hn2->set_neighbors(h23, h3n, h2n, vn, e2n, fn23);
+        hn3->set_neighbors(h31, h1n, h3n, vn, e3n, fn31);
+        h1n->set_neighbors(hn3, h31, hn1, v1, e1n, fn31);
+        h3n->set_neighbors(hn2, h23, hn3, v3, e3n, fn23);
+        h23->set_neighbors(h3n, hn2, h23->inv, v2, h23->edge, fn23);
+        h31->set_neighbors(h1n, hn3, h31->inv, v3, h31->edge, fn31);
+    }
 
+    // 初始化face
+    // 连接halfedge
     vn->halfedge = hn1;
     return vn;
 }
 
 optional<Vertex*> HalfedgeMesh::collapse_edge(Edge* e)
 {
-    // 要用到的半边
-    auto h12 = e->halfedge;
-    auto h21 = h12->inv;
-    auto h23 = h12->next, h32 = h23->inv;
-    auto h31 = h12->prev, h13 = h31->inv;
-    auto h14 = h21->next, h41 = h14->inv;
-    auto h42 = h21->prev, h24 = h42->inv;
-    erase(e);
-    erase(h12);
-    erase(h21);
-    erase(h23);
-    erase(h31);
-    erase(h14);
-    erase(h42);
-    auto h1 = h13, h2 = h24;
-    auto hinv1 = h32, hinv2 = h41;
-    auto hfrom2 = hinv1->next;
-    auto hfrom1 = hinv2->next;
-    // 要用到的边
-    auto e1n = h1->edge;
-    auto e2n = h2->edge;
+    logger->info("collapse begins");
+    // values to be destroyed
+    auto e12  = e;
+    auto h12  = e->halfedge;
+    auto h21  = h12->inv;
+    auto h23  = h12->next;
+    auto h31  = h12->prev;
+    auto h14  = h21->next;
+    auto h42  = h21->prev;
+    auto v1   = h12->from;
+    auto v2   = h21->from;
+    auto f123 = h12->face;
+    auto f124 = h21->face;
+    logger->info("values to be adjusted");
+    // values to be adjusted
+    auto h1    = h31->inv;
+    auto h2    = h42->inv;
+    auto hinv1 = h23->inv;
+    auto hinv2 = h14->inv;
+    auto e1    = h1->edge;
+    auto e2    = h2->edge;
+    // values to be destroyed
+    auto ed1 = hinv1->edge;
+    auto ed2 = hinv2->edge;
+    // values to be created
+    logger->info("new_vertex");
+    auto vn    = new_vertex();
+    vn->is_new = true;
+    // values to be used
+    auto v3      = hinv1->from;
+    auto v4      = hinv2->from;
+    auto hfromv1 = v1->halfedge;
+    auto hfromv2 = v2->halfedge;
 
-    e1n->halfedge = h1;
-    e2n->halfedge = h2;
-    // 要用到的顶点
-    auto v1 = h13->from;
-    auto v2 = h24->from;
-    auto v3 = h32->from;
-    auto v4 = h41->from;
-    erase(v1);
-    erase(v2);
-    v3->halfedge = hinv1;
-    v4->halfedge = hinv2;
-    auto vn      = new_vertex();
-    vn->pos      = (v3->pos + v4->pos) / 2;
-    vn->halfedge = h1;
-    h1->set_neighbors(h1->next, h1->prev, hinv1, vn, e1n, h1->face);
-    h2->set_neighbors(h2->next, h2->prev, hinv2, vn, e2n, h2->face);
-    hinv1->set_neighbors(hinv1->next, hinv1->prev, h1, v3, e1n, hinv1->face);
-    hinv2->set_neighbors(hinv2->next, hinv2->prev, h2, v4, e2n, hinv2->face);
-    while (hfrom1 != h1) {
-        hfrom1->from = vn;
-        hfrom1       = hfrom1->inv->next;
+    // adjustments
+    logger->info("adjustments");
+    auto v1degree = v1->degree();
+    auto v2degree = v2->degree();
+    logger->info("{} {}", v1degree, v2degree);
+    for (size_t i = 0; i < v1degree; i++) {
+        logger->info("hfromv1");
+        hfromv1->from = vn;
+        hfromv1       = hfromv1->inv->next;
     }
-    while (hfrom2 != h2) {
-        hfrom2->from = vn;
-        hfrom2       = hfrom2->inv->next;
+    logger->info("done");
+    for (size_t i = 0; i < v2degree; i++) {
+        logger->info("hfromv2");
+        hfromv2->from = vn;
+        hfromv2       = hfromv2->inv->next;
     }
+    logger->info("done");
+    if (h12->is_boundary()) {
+        logger->info("boundary......");
+        h12->face->halfedge = h12->next;
+        vn->pos             = e->center();
+        vn->halfedge        = h2;
+        h2->set_neighbors(h2->next, h2->prev, hinv2, vn, e2, h2->face);
+        hinv2->set_neighbors(hinv2->next, hinv2->prev, h2, hinv2->from, e2, hinv2->face);
+        erase(h21), erase(h14), erase(h42);
+        erase(e12), erase(ed2);
+        erase(f124);
+    } else if (h21->is_boundary()) {
+        logger->info("boundary......");
+        h21->face->halfedge = h21->next;
+        vn->pos             = e->center();
+        vn->halfedge        = h1;
+        h1->set_neighbors(h1->next, h1->prev, hinv1, vn, e1, h1->face);
+        hinv1->set_neighbors(hinv1->next, hinv1->prev, h1, hinv1->from, e1, hinv1->face);
+        erase(h12), erase(h23), erase(h31);
+        erase(e12), erase(ed1);
+        erase(f123);
+    } else {
+        h1->set_neighbors(h1->next, h1->prev, hinv1, vn, e1, h1->face);
+        h2->set_neighbors(h2->next, h2->prev, hinv2, vn, e2, h2->face);
+        hinv1->set_neighbors(hinv1->next, hinv1->prev, h1, hinv1->from, e1, hinv1->face);
+        hinv2->set_neighbors(hinv2->next, hinv2->prev, h2, hinv2->from, e2, hinv2->face);
+        e1->halfedge = h1;
+        e2->halfedge = h2;
+        vn->pos      = (v3->pos + v4->pos) / 2;
+        vn->halfedge = h1;
+        // destroy values
+        logger->info("destroying...");
+        erase(h12), erase(h21), erase(h23), erase(h31), erase(h14), erase(h42);
+        erase(ed1), erase(ed2), erase(e12);
+        erase(f123), erase(f124);
+    }
+    erase(v1), erase(v2);
+    logger->info("return");
     return vn;
 }
 
@@ -338,16 +392,97 @@ void HalfedgeMesh::isotropic_remesh()
     //    been destroyed by a collapse (which ones?)
     // -> Now flip each edge if it improves vertex degree
     // -> Finally, apply some tangential smoothing to the vertex positions
+    // 计算平均边长
+    // 重复以下步骤5～6次
+    // - 将比目标长度长得多的边分开（写循环时要注意
+    // - 将比目标长度短得多的边坍缩掉。这里循环需要非常小心，因为许多边可能已经被摧毁掉了
+    // - 翻转每个会增加结点度数的边
+    // - 最后对节点位置作优化
     static const size_t iteration_limit = 5;
     set<Edge*> selected_edges;
+    double average_edge_length = 0;
+    for (auto e = edges.head; e != nullptr; e = e->next_node) {
+        selected_edges.insert(e);
+        average_edge_length += e->length();
+        logger->info("adding edges...:{}", (e)->length());
+    }
+    average_edge_length /= edges.size;
+    auto up_lim   = average_edge_length * 4.0f / 3;
+    auto down_lim = average_edge_length * 4.0f / 5;
     for (size_t i = 0; i != iteration_limit; ++i) {
-        // Split long edges.
+        vector<Edge*> save_delete_edges;
+        vector<Edge*> save_add_edges;
+        for (auto pe = selected_edges.begin(); pe != selected_edges.end(); ++pe) {
+            // 分开长边
+            logger->info("split?:{}", (*pe)->length());
+            if (((*pe)->length() > up_lim)) {
+                logger->info("spliting...");
+                auto vn = split_edge(*pe).value_or(nullptr);
+                auto e1 = vn->halfedge->edge;
+                auto e2 = vn->halfedge->prev->edge;
+                auto e3 = vn->halfedge->prev->inv->next->edge;
+                auto e4 = vn->halfedge->inv->next->edge;
+                save_delete_edges.push_back(*pe);
+                save_add_edges.push_back(e1);
+                save_add_edges.push_back(e2);
+                save_add_edges.push_back(e3);
+                save_add_edges.push_back(e4);
+            }
+        }
+        for (auto pe : save_add_edges) {
+            selected_edges.erase(pe);
+        }
+        for (auto pe : save_delete_edges) {
+            selected_edges.insert(pe);
+        }
+        for (auto pe = selected_edges.rbegin(); pe != selected_edges.rend(); ++pe) {
+            // 摧毁短边
+            logger->info("colloapse?:{}", (*pe)->length());
+            if (((*pe)->length() < down_lim)) {
+                auto hinv1 = (*pe)->halfedge->next;
+                auto hinv2 = (*pe)->halfedge->inv->next;
+                auto e14   = hinv1->edge;
+                auto e42   = (*pe)->halfedge->prev->edge;
+                auto e23   = hinv2->edge;
+                auto e31   = (*pe)->halfedge->inv->prev->edge;
+                save_delete_edges.push_back(*pe);
+                save_delete_edges.push_back(e14);
+                save_delete_edges.push_back(e42);
+                save_delete_edges.push_back(e23);
+                save_delete_edges.push_back(e31);
+                logger->info("colloapsing...");
+                collapse_edge(*pe);
+                auto e1 = hinv1->edge;
+                auto e2 = hinv2->edge;
+                save_add_edges.push_back(e1);
+                save_add_edges.push_back(e2);
+            }
+        }
+        for (auto pe : save_add_edges) {
+            selected_edges.erase(pe);
+        }
+        for (auto pe : save_delete_edges) {
+            selected_edges.erase(pe);
+        }
+        save_add_edges.clear();
+        save_delete_edges.clear();
 
-        // Collapse short edges.
-
-        // Flip edges.
-
-        // Vertex averaging.
+        // 翻转边
+        for (auto e = edges.head; e != nullptr; e = e->next_node) {
+            auto v1 = e->halfedge->from;
+            auto v2 = e->halfedge->inv->from;
+            auto v3 = e->halfedge->next->from;
+            auto v4 = e->halfedge->inv->next->from;
+            if (std::abs(v1->degree() - 6.0f) + std::abs(v2->degree() - 6.0f) +
+                    std::abs(v3->degree() - 6.0f) + std::abs(v4->degree() - 6.0f) >
+                std::abs(v1->degree() - 7.0f) + std::abs(v2->degree() - 7.0f) +
+                    std::abs(v3->degree() - 5.0f) + std::abs(v4->degree() - 5.0f)) {
+                flip_edge(e);
+            }
+        }
+        for (auto v = vertices.head; v != nullptr; v = v->next_node) {
+            // 将节点平均化
+        }
     }
     logger->info("remeshed mesh: {} vertices, {} faces\n", vertices.size, faces.size);
     global_inconsistent = true;

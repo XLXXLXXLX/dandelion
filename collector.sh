3de086e1dc2c3e613cccfd9b099432d22de78fdb
XLXXLXXLX <1294694867@qq.com>
2023-11-15 17:26:59 +0800

    empty

--- collector.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/zsh
-
-mkdir -p done
-zip -r src.zip src
-mv src.zip done
-mv screenshots.zip done
-cp modifications.cpp done
--- test/builder.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/zsh
-
-mkdir -p build
-cd build
-CC=gcc-12 CXX=g++-12 cmake -S .. -B . -DCMAKE_BUILD_TYPE=Debug
-CC=gcc-12 CXX=g++-12 cmake --build . --parallel 8

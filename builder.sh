8654a8758e2cc261181a0966b040e4886e141803
XLXXLXXLX <1294694867@qq.com>
2023-11-16 17:45:47 +0800

    add libs

--- builder.sh
+++ builder.sh
@@ -1,15 +1,17 @@
 # !/bin/zsh
 
-mkdir -p build
-cd build
-if [["$#" == 0]]; then
+if [[ "$1" == "debug" ]]; then
+    mkdir -p build
+    cd build
     CC=gcc-12 CXX=g++-12 cmake -S .. -B . -DCMAKE_BUILD_TYPE=Debug
-elif [[ "$1" == "debug" ]]; then
-    CC=gcc-12 CXX=g++-12 cmake -S .. -B . -DCMAKE_BUILD_TYPE=Debug
+    CC=gcc-12 CXX=g++-12 cmake --build . --parallel 8
 elif [[ "$1" == "release" ]]; then
+    mkdir -p build
+    cd build
     CC=gcc-12 CXX=g++-12 cmake -S .. -B . -DCMAKE_BUILD_TYPE=Release
+    CC=gcc-12 CXX=g++-12 cmake --build . --parallel 8
+elif [[ "$1" == "clean" ]]; then
+    rm -rf build
 else
-    echo "Invalid argument. Usage: ./builder.sh [debug|release]"
+    echo "Invalid argument. Usage: ./builder.sh [debug|release|clean]"
 fi
-
-CC=gcc-12 CXX=g++-12 cmake --build . --parallel 8
